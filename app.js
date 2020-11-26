b662c4473dc88f36b72ebcb5546305149c238043
Gummy27 <brimir27@outlook.com>
2020-11-26 16:36:34 +0000

    Quick sort er klárað. Nú er hægt að stoppa insertion og bubble sort

--- app.js
+++ app.js
@@ -18,43 +18,108 @@ function swapTags(lower, higher) {
     }
 }
 
+// Bubble sort
 async function bubbleSort() {
-    while(true) {
-        let comp = 0;
-        for(let x = 0; x < children.length-1; x++) {
-            selectedChild = children[x]
+    if(!running) {
+        running = true
+        while(true && running) {
+            let comp = 0;
+            for(let x = 0; x < children.length-1; x++) {
+                if(!running) {
+                    break;
+                } 
+
+                selectedChild = children[x]
+                selectedChild.style.backgroundColor = "blue";
+                if(parseInt(children[x].dataset.value) > parseInt(children[x+1].dataset.value)) {
+                    swapTags(x, x+1)
+                    await timer(1);
+                    comp += 1
+                }
+                selectedChild.style.backgroundColor = "red";
+            }
+            if(comp == 0) {
+                break
+            }
+        }
+        if(running) {
+            victoryLap();
+        }
+        running = false
+    }
+
+}
+
+// Insertion sort
+async function insertionSort() {
+    if(!running) {
+        running = true
+        let holeposition;
+        for(let selected = 1; selected < children.length; selected++) {
+            if(!running) {
+                break;
+            }
+
+            holeposition = selected;
+            selectedChild = children[selected]
             selectedChild.style.backgroundColor = "blue";
-            if(parseInt(children[x].dataset.value) > parseInt(children[x+1].dataset.value)) {
-                swapTags(x, x+1)
-                await timer(1);
-                comp += 1
+            while(holeposition > 0 && parseInt(selectedChild.dataset.value) < parseInt(children[holeposition-1].dataset.value) && running) {
+                swapTags(holeposition, holeposition-1)
+                await timer(10);
+                holeposition--
             }
             selectedChild.style.backgroundColor = "red";
         }
-        if(comp == 0) {
-            break
+        if(running) {
+            victoryLap();
         }
+        running = false
     }
-
-    victoryLap();
 }
 
-async function insertionSort() {
-    let holeposition;
-    for(let selected = 1; selected < children.length; selected++) {
-        holeposition = selected;
-        selectedChild = children[selected]
-        selectedChild.style.backgroundColor = "blue";
-        while(holeposition > 0 && parseInt(selectedChild.dataset.value) < parseInt(children[holeposition-1].dataset.value)) {
-            swapTags(holeposition, holeposition-1)
-            await timer(10);
-            holeposition--
-        }
-        selectedChild.style.backgroundColor = "red";
+// Quick sort
+async function quickSort(low=0, high=children.length-1) {
+    if(low < high) {
+        pi = await partition(low, high)
+        await quickSort(low, pi-1)
+        await quickSort(pi+1, high)
     }
-    victoryLap();
 }
 
+async function partition(low, high) {
+    pivot = children[high].dataset.value;
+    i = (low - 1);
+    for(j = low; j <= high - 1; j++) {
+        if(parseInt(children[j].dataset.value) < parseInt(pivot)) {
+            await timer(1);
+            i++
+            swapTags(i, j)
+        }
+    }
+    await timer(1);
+    swapTags(i+1, high)
+    return i + 1;
+}
+
+// Radix sort
+async function radixSort() {
+    max_element = children.length - 1;
+
+    for(let place = 1; max_element / place > 0; place *= 10) {
+        countingSort(place)
+    }
+}
+
+async function countingSort(place) {
+    return
+}
+
+
+/*
+
+Hérna koma önnur föll
+
+*/
 async function victoryLap() {
     for(let x = 1; x < children.length-1; x++) {
         children[x-1].style.backgroundColor = "lightgreen";
@@ -71,6 +136,7 @@ async function victoryLap() {
         children[children.length-1].style.backgroundColor = "red";
     }, 500);
 }
+
 
 function printOutDisplay() {
     let listi = []
@@ -93,21 +159,21 @@ function drawGraphics() {
     let list = [...Array(parseInt(document.forms['values']["size"].value)).keys()];
     document.getElementById("graph").innerHTML = "";
     for(let x = 0; x < list.length; x++) {
-        document.getElementById("graph").append(bar(list[x], (list[x]+1)*5));
+        document.getElementById("graph").append(bar(list[x], (list[x]+1)*parseFloat(document.forms['values']["arrSize"].value)));
     }
 }
 
 function bar(value, height) {
     let newDiv = document.createElement("div");
     newDiv.style.height = height + "px";
-    newDiv.style.width = "5px";
+    newDiv.style.width = parseFloat(document.forms['values']["arrSize"].value) + "px";
     newDiv.style.backgroundColor = "Red";
-    newDiv.style.marginTop = (window.innerHeight - 20 - height) + "px"
+    newDiv.style.marginTop = (window.innerHeight - 40 - height) + "px"
     newDiv.setAttribute("data-value", value)
 
     return newDiv;
 }
 
 document.getElementById("quantity").addEventListener("input", drawGraphics);
-
+document.getElementById("sizeOfArr").addEventListener("input", drawGraphics);
 drawGraphics();
