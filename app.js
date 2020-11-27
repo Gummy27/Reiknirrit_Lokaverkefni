11ca2d6d3971cdf5007a9f0b07846e585490b20b
Gummy27 <brimir27@outlook.com>
2020-11-27 18:51:57 +0000

    Radix sort var klárað. Ui var aðeins breytt

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let running = false
 let sortVisual = document.getElementById("graph");
 let children = sortVisual.childNodes;
 
+let mainColour = "red";
+let activeColour = "blue";
+let victoryColour = "#43cc1a";
+
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
 function swapTags(lower, higher) {
@@ -30,13 +34,13 @@ async function bubbleSort() {
                 } 
 
                 selectedChild = children[x]
-                selectedChild.style.backgroundColor = "blue";
+                selectedChild.style.backgroundColor = activeColour;
                 if(parseInt(children[x].dataset.value) > parseInt(children[x+1].dataset.value)) {
                     swapTags(x, x+1)
                     await timer(1);
                     comp += 1
                 }
-                selectedChild.style.backgroundColor = "red";
+                selectedChild.style.backgroundColor = mainColour;
             }
             if(comp == 0) {
                 break
@@ -62,13 +66,13 @@ async function insertionSort() {
 
             holeposition = selected;
             selectedChild = children[selected]
-            selectedChild.style.backgroundColor = "blue";
+            selectedChild.style.backgroundColor = activeColour;
             while(holeposition > 0 && parseInt(selectedChild.dataset.value) < parseInt(children[holeposition-1].dataset.value) && running) {
                 swapTags(holeposition, holeposition-1)
                 await timer(10);
                 holeposition--
             }
-            selectedChild.style.backgroundColor = "red";
+            selectedChild.style.backgroundColor = mainColour;
         }
         if(running) {
             victoryLap();
@@ -87,10 +91,12 @@ async function quickSort(low=0, high=children.length-1) {
 }
 
 async function partition(low, high) {
-    pivot = children[high].dataset.value;
+    pivot = children[high];
+    pivot.style.backgroundColor = activeColour;
+
     i = (low - 1);
     for(j = low; j <= high - 1; j++) {
-        if(parseInt(children[j].dataset.value) < parseInt(pivot)) {
+        if(parseInt(children[j].dataset.value) < parseInt(pivot.dataset.value)) {
             await timer(1);
             i++
             swapTags(i, j)
@@ -98,6 +104,7 @@ async function partition(low, high) {
     }
     await timer(1);
     swapTags(i+1, high)
+    pivot.style.backgroundColor = mainColour;
     return i + 1;
 }
 
@@ -105,15 +112,56 @@ async function partition(low, high) {
 async function radixSort() {
     max_element = children.length - 1;
 
-    for(let place = 1; max_element / place > 0; place *= 10) {
-        countingSort(place)
+    for(let place = 1; Math.floor(max_element / place) > 0; place *= 10) {
+        await countingSort(place)
+        printOutDisplay();
     }
+    victoryLap();
 }
 
 async function countingSort(place) {
-    return
+    count = new Array(10);
+    output = new Array(children.length)
+    for(let x = 0; x < count.length; x++) { count[x] = 0; }
+
+    for(let i = 0; i < children.length; i++) {
+        children[i].style.backgroundColor = activeColour;
+
+        index = (children[i].dataset.value / place)
+        count[Math.floor(index % 10)]++;
+        console.log("This is the count:", count, index % 10);
+        await timer(1);
+
+        children[i].style.backgroundColor = mainColour;
+    }
+
+    for(let i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    i = children.length - 1;
+    while(i >= 0) {
+        index = (children[i].dataset.value / place);
+        output[count[Math.floor(index % 10)] - 1] = children[i].dataset.value;
+        count[Math.floor(index % 10)]--;
+        i--;
+    }
+
+    for(let i = 0; i < children.length; i++) {
+        if(output[i]) {
+            await timer(1);
+            swapTags(i, findByValue(output[i]));
+        }
+    }
 }
 
+function findByValue(value) {
+    for(let i = 0; i < children.length; i++) {
+        if(parseInt(children[i].dataset.value) == value) {
+            return i;
+        }
+    }
+}
 
 /*
 
@@ -122,18 +170,18 @@ Hérna koma önnur föll
 */
 async function victoryLap() {
     for(let x = 1; x < children.length-1; x++) {
-        children[x-1].style.backgroundColor = "lightgreen";
-        children[x].style.backgroundColor = "lightgreen";
-        children[x+1].style.backgroundColor = "lightgreen";
+        children[x-1].style.backgroundColor = victoryColour;
+        children[x].style.backgroundColor = victoryColour;
+        children[x+1].style.backgroundColor = victoryColour;
 
         setTimeout(function() {
-            children[x-1].style.backgroundColor = "red";
+            children[x-1].style.backgroundColor = mainColour;
         }, 500);
         await timer(1);
     }
     setTimeout(function() {
-        children[children.length-2].style.backgroundColor = "red";
-        children[children.length-1].style.backgroundColor = "red";
+        children[children.length-2].style.backgroundColor = mainColour;
+        children[children.length-1].style.backgroundColor = mainColour;
     }, 500);
 }
 
@@ -165,10 +213,14 @@ function drawGraphics() {
 
 function bar(value, height) {
     let newDiv = document.createElement("div");
+
     newDiv.style.height = height + "px";
     newDiv.style.width = parseFloat(document.forms['values']["arrSize"].value) + "px";
-    newDiv.style.backgroundColor = "Red";
-    newDiv.style.marginTop = (window.innerHeight - 40 - height) + "px"
+    newDiv.style.backgroundColor = mainColour;
+    newDiv.style.marginTop = (window.innerHeight - 40 - height) + "px";
+    if(document.forms['values']["arrSize"].value > 5) {
+        newDiv.style.marginRight = "1px";
+    }
     newDiv.setAttribute("data-value", value)
 
     return newDiv;
